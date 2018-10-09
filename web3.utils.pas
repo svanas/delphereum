ca9c3bd4794936ef6146510542b600111b435906
svanas <svanas@runbox.com>
2018-10-09 14:59:25 +0200

    Added: web3.utils.sha3()

--- web3.utils.pas
+++ web3.utils.pas
@@ -5,8 +5,13 @@ interface
 uses
   // Delphi
   System.SysUtils,
+  System.JSON,
   // Velthuis' BigNumbers
-  Velthuis.BigIntegers;
+  Velthuis.BigIntegers,
+  // Web3
+  web3,
+  web3.json,
+  web3.json.rpc;
 
 type
   TEthUnit = (
@@ -35,10 +40,52 @@ type
     gether,
     tether);
 
+type
+  TASyncString = reference to procedure(const str: string; err: Exception);
+
+function toHex(const buf: TBytes): string; overload;
+function toHex(const str: string): string; overload;
+
+procedure sha3(client: TWeb3; const hex: string; callback: TASyncString);
+
 function fromWei(wei: BigInteger; &unit: TEthUnit): string; overload;
 function fromWei(wei: BigInteger; &unit: TEthUnit; const aFormatSettings: TFormatSettings): string; overload;
 
 implementation
+
+function toHex(const buf: TBytes): string;
+const
+  Digits = '0123456789ABCDEF';
+var
+  I: Integer;
+begin
+  SetLength(Result, Length(buf) * 2);
+  try
+    for I := 0 to Length(buf) - 1 do
+    begin
+      Result[2 * I + 1] := Digits[(buf[I] shr 4)  + 1];
+      Result[2 * I + 2] := Digits[(buf[I] and $F) + 1];
+    end;
+  finally
+    Result := '0x' + Result;
+  end;
+end;
+
+function toHex(const str: string): string;
+begin
+  Result := toHex(TEncoding.UTF8.GetBytes(str));
+end;
+
+procedure sha3(client: TWeb3; const hex: string; callback: TASyncString);
+begin
+  web3.json.rpc.Send(client.URL, 'web3_sha3', [hex], procedure(resp: TJsonObject; err: Exception)
+  begin
+    if Assigned(err) then
+      callback('', err)
+    else
+      callback(web3.json.GetPropAsStr(resp, 'result'), nil);
+  end);
+end;
 
 const
   UnitToWei: array[TEthUnit] of string = (
